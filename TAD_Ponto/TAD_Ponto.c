bc3c7f1e5fde9f3f1dd1f9dc61ec6c4a85e186a3
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2024-12-20 16:28:30 -0300

    Inicio TAD

---
+++ TAD_Ponto/TAD_Ponto.c
@@ -0,0 +1,12 @@
+#ifndef __TAD_PONTO_C__
+#define __TAD_PONTO_C__
+
+PPonto cria_ponto(float x, float y){
+    PPonto ponto = (PPonto)malloc(sizeof(Ponto));
+    ponto->x=x;
+    ponto->y=y;
+
+    return ponto;
+}
+
+#endif
---
+++ TAD_Ponto/TAD_Ponto.h
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+typedef struct ponto{
+    float x;
+    float y;
+}Ponto;
+typedef Ponto * PPonto;
+
+PPonto ponto_cria(float x,float y);
+
+#include "TAD_Ponto.c";
