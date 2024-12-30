0720e17cfeb67de6214009aa70ed0310bd34ceae
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2024-12-30 14:41:15 -0300

    atualiza

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(){
 
-    PPonto a = cria_ponto( 0, 0);
-    printf("( %.2f, %.2f)", a->x, a->y);
+    PPonto a = cria_ponto( 2, 0);
+    printf("(%.2f, %.2f)", a->x, a->y);
 
     free(a);
     
